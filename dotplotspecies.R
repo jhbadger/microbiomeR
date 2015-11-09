792a57415697df8267bd79b0dbf6faf3235083f1
Jonathan Badger <jhbadger@gmail.com>
2015-11-09 18:15:26 -0500

    includeSpecies

--- dotplotspecies.R
+++ dotplotspecies.R
@@ -2,7 +2,8 @@
 # inputs: tax.summary file from Mothur, taxon string, group to group by
 # optional arrays of samples to exclude (e.g. Mock)
 
-dotplotspecies = function(taxSummaryFile, taxon, group=c(), excludeSamples=c(),normalize=TRUE) {
+dotplotspecies = function(taxSummaryFile, taxon, group=c(), excludeSamples=c(),includeSamples=c(),
+                          normalize=TRUE) {
   library(ggplot2)
   library(reshape2)
   counts = read.table(taxSummaryFile, header = TRUE)
@@ -10,7 +11,12 @@ dotplotspecies = function(taxSummaryFile, taxon, group=c(), excludeSamples=c(),n
   rownames(counts) = counts$taxon
   # get rid of excess columns in mothur tax.summary file
   counts = counts[,-grep("taxon|taxlevel|rankID|daughterlevels|total", colnames(counts))]
-  counts = counts[,!colnames(counts) %in% excludeSamples]
+  if (length(includeSamples) > 0 ) {
+    counts = counts[,colnames(counts) %in% includeSamples]
+  }
+  else {
+    counts = counts[,!colnames(counts) %in% excludeSamples]
+  }
   if (normalize) {
     counts = round((counts/rowSums(counts))*100000)
   }
@@ -20,10 +26,8 @@ dotplotspecies = function(taxSummaryFile, taxon, group=c(), excludeSamples=c(),n
   counts=melt(counts)
   counts$group = group
   colnames(counts) = c("Sample", "Value","Group")
-  plot = ggplot(counts,aes(x=Group,y=Value)) + geom_dotplot(binaxis = "y", stackdir = "center")
-  plot = plot + theme(axis.text.x=element_text(angle=-90,hjust=0))
-  plot = plot + theme(legend.position="none")
-  plot = plot + scale_x_discrete(name="")
-  plot = plot + scale_y_continuous("Taxon Counts") + ggtitle(taxon)
-  plot
+  ggplot(counts,aes(x=Group,y=Value)) + geom_dotplot(binaxis = "y", stackdir = "center") +
+    theme(axis.text.x=element_text(angle=-90,hjust=0)) +
+    theme(legend.position="none") +scale_x_discrete(name="") + scale_y_continuous("Taxon Counts") + 
+    ggtitle(taxon)
 }
